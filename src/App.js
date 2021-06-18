906602c21d431ba18e48b5036cc7e4b8411666f4
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-18 15:38:21 +0200

    Messages with Material-UI cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 import './App.css';
 import Message from "./Message";
@@ -7,15 +7,29 @@ import Message from "./Message";
 function App() {
     //State aanmaken
     const [input, setInput] = useState('');
-    const [messages, setMessages] = useState(['Hey!', 'Thanks', 'voor je Post!']);
+    const [messages, setMessages] = useState([
+        {username: 'sonny', text: 'hey guys'},
+        {username: 'qazi', text: 'Whats up'}
+    ]);
+    //Stukje state om de user te onthouden
+    const [username, setUsername] = useState('');
 
-    console.log(input);
-    console.log(messages);
+    //Geheugensteuntje:
+    // useState = variable in REACT
+    // useEffect = run code on a condition in REACT
+
+    useEffect(() => {
+        setUsername(prompt('Please enter your name'))
+    }, []) //(de condition)
+    //(If its blank inside [], this code runs ONCE when the app component loads)
+
 
     const sendMessage = (event) => {
         //All the logic to send a message goes here
         event.preventDefault(); //(zorgt ervoor dat het niet refreshed)
-        setMessages([...messages, input]);
+        setMessages([
+            ...messages, {username: username, text: input}
+            ]);
         setInput('');
 
     }
@@ -23,6 +37,7 @@ function App() {
   return (
     <div className="App">
       <h1>Startpunt!</h1>
+      <h2>Welcome {username}</h2>
 
       <form>
           <FormControl>
@@ -36,11 +51,9 @@ function App() {
       </form>
 
 
-      {/* messages themselves */}
-
         {
             messages.map(message => (
-                <Message text={message} />
+                <Message username={message.username} text={message.text} />
 
             ))
         }
--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Card, CardContent, Typography} from "@material-ui/core";
+
+
+//Dit keer material-UI gebruiken voor de styling
 
 function Message(props) {
     return (
-        <div>
-            <h2>{props.text}</h2>
-        </div>
+            <Card>
+                <CardContent>
+                    <Typography
+                        color="white"
+                        variant="h5"
+                        component="h2"
+                    >
+                        {props.username}: {props.text}
+                    </Typography>
+                </CardContent>
+            </Card>
     )
 }
 
