7979f4276f402cb4602ed0a57afad3d85800d4df
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-18 13:23:08 +0200

    States added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
 
-//initial
 
 function App() {
     //State aanmaken
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState(['Hey!', 'Thanks', 'voor je Post!']);
+
+    console.log(input);
+    console.log(messages);
+
+    const sendMessage = (event) => {
+        //All the logic to send a message goes here
+        event.preventDefault(); //(zorgt ervoor dat het niet refreshed)
+        setMessages([...messages, input]);
+        setInput('');
+
+    }
 
   return (
     <div className="App">
       <h1>Startpunt!</h1>
 
-      <input />
-      <button>Send Message</button>
-      {/* input field */}
-      {/* button */}
+      <form>
+          <input value={input} onChange={event => setInput(event.target.value)} />
+          <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+
 
       {/* messages themselves */}
+
+        {
+            messages.map(message => (
+                <p>{message}</p>
+            ))
+        }
+
     </div>
   );
 }
